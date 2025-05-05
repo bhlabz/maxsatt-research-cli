c2195a3c506e06eb3c6c4900ff11d366765b6dde
GabiHert <hertergabriel4@gmail.com>
2025-05-04 22:21:00 -0300

    fix .env path

--- python-service/main.py
+++ python-service/main.py
@@ -108,7 +108,7 @@ def serve():
 
 if __name__ == "__main__":
     # Load environment variables from .env file
-    env_path = os.path.join(os.path.dirname(__file__), '../../.env')
+    env_path = os.path.join(os.path.dirname(__file__), '../.env')
     load_dotenv(env_path)
     print(f"ROOT_PATH: {os.getenv('ROOT_PATH')}")
     serve()
