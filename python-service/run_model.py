8e7c70286bbd29152749a7dbb773b238d09106a5
GabiHert <hertergabriel4@gmail.com>
2025-05-16 13:37:30 -0300

    Refactor run_model.py to correctly apply the reflectance model after climate group modeling

--- python-service/run_model.py
+++ python-service/run_model.py
@@ -10,9 +10,9 @@ def run_model(model, input, climate_group_clusters=2, reflectance_clusters=16):
     dataset = pd.read_csv(f'{root}/data/model/{model}')
     dataset_concat = pd.concat([dataset, input], ignore_index=True)
 
-    result = climate_group_model(dataset_concat, climate_group_clusters)
-    if len(result['label'].unique()) == 1:
-        print("Only one cluster was found, skipping reflectance model")
-        return 
-    result = reflectance_model(result, reflectance_clusters)
+    # result = climate_group_model(dataset_concat, climate_group_clusters)
+    # if len(result['label'].unique()) == 1:
+    #     print("Only one cluster was found, skipping reflectance model")
+    #     return 
+    result = reflectance_model(dataset_concat, reflectance_clusters)
     return result
